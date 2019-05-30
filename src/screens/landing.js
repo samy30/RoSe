1e5b4c69c7405736874c497df7164c208c1d8198
samy30 <samibelaid30@gmail.com>
2019-05-30 08:23:29 +0100

    2nd

--- src/screens/landing.js
+++ src/screens/landing.js
@@ -1,32 +1,34 @@
 import React, { Component } from "react";
 import Carousel from "react-spring-3d-carousel";
+import {Button} from "react-bootstrap"
 import uuidv4 from "uuid";
 import { config } from "react-spring";
+import map from "../assets/images/map2.png"
 
 let slides = [
   {
     key: uuidv4(),
-    content: <img src="https://picsum.photos/800/801/?random" alt="1" />
+    content: <div><img src={map} width={300} height={300} alt="1" /><h1>Maps</h1></div>
   },
   {
     key: uuidv4(),
-    content: <img src="https://picsum.photos/800/802/?random" alt="2" />
+    content: <img src="https://picsum.photos/400/402/?random" alt="2" />
   },
   {
     key: uuidv4(),
-    content: <img src="https://picsum.photos/600/803/?random" alt="3" />
+    content: <img src="https://picsum.photos/400/403/?random" alt="3" />
   },
   {
     key: uuidv4(),
-    content: <img src="https://picsum.photos/800/500/?random" alt="4" />
+    content: <img src="https://picsum.photos/400/400/?random" alt="4" />
   },
   {
     key: uuidv4(),
-    content: <img src="https://picsum.photos/800/804/?random" alt="5" />
+    content: <img src="https://picsum.photos/400/404/?random" alt="5" />
   },
   {
     key: uuidv4(),
-    content: <img src="https://picsum.photos/500/800/?random" alt="6" />
+    content: <img src="https://picsum.photos/400/400/?random" alt="6" />
   },
   {
     key: uuidv4(),
@@ -39,12 +41,21 @@ let slides = [
 ];
 
 export default class Landing extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.testing = this.testing.bind(this);
+  }
   state = {
     goToSlide: 0,
     offsetRadius: 2,
     showNavigation: true,
     config: config.gentle
   };
+
+  testing() {
+    this.setState({goToSlide : (this.state.goToSlide-1)%8 })
+  }
 
  
 
@@ -58,6 +69,7 @@ export default class Landing extends Component {
           showNavigation={this.state.showNavigation}
           animationConfig={this.state.config}
         />
+        <Button onClick={this.testing}>test</Button>
         </div>
     );
   }
